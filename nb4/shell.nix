f4b0f9611bfc3c2b89bf12f11fea57a3119e4dbd
Dan Connolly <connolly@agoric.com>
2021-07-28 13:27:37 -0500

    chore: add statsmodels, readline, etc. to nix config

--- nb4/shell.nix
+++ nb4/shell.nix
@@ -1,10 +1,15 @@
 # https://nixos.wiki/wiki/Development_environment_with_nix-shell
+# https://nixos.wiki/wiki/Packaging/Python
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = [
       # pkgs.python38Packages.pyspark
+      pkgs.readline
       pkgs.jupyter
+      pkgs.python38Packages.pip
+      pkgs.python38Packages.virtualenv
+      pkgs.python38Packages.statsmodels
       pkgs.python38Packages.jupytext
       pkgs.python38Packages.pandas pkgs.python38Packages.matplotlib
       pkgs.python38Packages.dask
