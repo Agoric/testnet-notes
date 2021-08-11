5fbc01c2602f8785d639b2ae6a631026c998e07a
Dan Connolly <connolly@agoric.com>
2021-08-11 11:00:25 -0500

    feat: bank_trace etc.

--- nb4/run_policy_simulation.py
+++ nb4/run_policy_simulation.py
@@ -100,6 +100,8 @@ _dc16.tail()
 
 # +
 def simulate_run_policy(df, threshold=8e6):
+    # does 10e6 help with bank update latency?
+    # only a little: max ~50 rather than ~70
     meter = 0
     block_in = None
     block_out = None
@@ -107,12 +109,12 @@ def simulate_run_policy(df, threshold=8e6):
         if block_in is None:
             block_in = block_out = d.blockHeight
             t_in = t_out = d.blockTime
+        yield block_out  # do the work
         if d.blockHeight > block_in:
             block_in = d.blockHeight
             if block_in > block_out:
                 block_out = block_in
                 meter = 0
-        yield block_out  # do the work
         meter += d.compute
         if meter > threshold:
             meter = 0
@@ -155,28 +157,187 @@ df[['vatID', 'deliveryNum',
 
 df[['durationInBlock', 'durationInSimBlk']].plot(figsize=(12, 6));
 
+# ## Latency
+
+df['delay'] = df.bkSim - df.blockHeight
+df[['delay']].describe()
+
+df[['durationInBlock', 'durationInSimBlk', 'delay']].plot(figsize=(12, 4))
+
+df.sort_values('delay', ascending=False).head(50)
+
 # ## Zoom in on the X axis
 
+103200 and 105500
+
 # +
-_zoom = df.loc[110000:112000]
+_zoom = df.loc[103200:105500]
 _zoom = _zoom.reset_index().set_index(['blockHeight', 'crankNum'])
 
 _zoom[['computeInBlock', 'computeInSimBlk']].plot(figsize=(12, 4), rot=-75);
 # -
 
+x = _zoom.reset_index()
+g = x.groupby('bkSim')
+x = pd.concat([
+    g[['blockHeight']].min(),
+    g[['compute']].sum(),
+    g[['dur']].sum()
+])
+x = x.assign(delay=x.index - x.blockHeight)
+x
+
+x[['compute', 'dur', 'delay']].plot(subplots=True, figsize=(15, 9))
+
+_zoom[['durationInBlock', 'durationInSimBlk']].plot(figsize=(12, 4), rot=-75);
+
+(df.bkSim - df.blockHeight).describe()
+
+(df.bkSim - df.blockHeight).hist(figsize=(10, 5), bins=72, log=True)
+
 # ## Elapsed time* on the X axis
 #
 # *estimated as cumulative crank duration
 
-_zoom = df.loc[110000:112000].copy()
+_zoom = df.loc[103273:104400].copy()
 # _zoom = df
 _zoom['t'] = _zoom.dur.cumsum()
-_zoom.set_index('t')[['computeInBlock', 'computeInSimBlk']].plot(figsize=(12, 4), rot=-75);
+_zoom.set_index('t')[['durationInBlock', 'durationInSimBlk']].plot(figsize=(12, 4), rot=-75);
 
 # ### Detailed Data
 
-_zoom.groupby('bkSim').apply(lambda g: g.head(10))[:50][[
+_zoom.groupby('bkSim').apply(lambda g: g.head(10))[50:100][[
     'dur', 't', 'durationInSimBlk', 'durationInBlock',
     'compute', 'computeInSimBlk', 'computeInBlock',
-    'blockHeight'
+    'blockHeight', 'delay'
 ]]
+
+df.loc[103200:105500][['delay']].plot()
+
+x = pd.read_sql('''
+select *
+from j_delivery
+where crankNum between 103200 and 105500
+and file_id = 3288529541296525
+''', _db4)
+x
+
+show_times(x[x.index.isin([x.index.min(), x.index.max()])])[['crankNum', 'blockHeight', 'blockTime']]
+
+x.blockHeight.max() - x.blockHeight.min()
+
+x.blockHeight.describe()
+
+x[x.compute > 1000000].groupby('method')[['compute', 'dur']].aggregate(['count', 'median', 'mean'])
+
+x = pd.read_sql('''
+select *
+from t_delivery
+where method = 'fromBridge'
+and blockHeight between 68817 and 69707
+and file_id = 3288529541296525
+''', _db4)
+x
+
+_db4.execute('''
+create index if not exists slog_entry_bk_ix on slog_entry(blockHeight)
+''');
+
+_db4.execute('drop index if exists slog_entry_ty_ix on slog_entry');
+
+
+# +
+def bank_trace(db,
+               limit=250,
+               file_id=3288529541296525,
+               bk_lo=68817,
+               bk_hi=69707):
+    df = pd.read_sql(
+        '''
+        with d as (
+        select file_id, run_line_lo
+         , line
+         , blockHeight
+         , blockTime
+         , time
+         , crankNum
+         , cast(substr(json_unquote(json_extract(record, '$.vatID')), 2) as int) vatID
+         , coalesce(cast(json_extract(record, '$.deliveryNum') as int), -1) deliveryNum
+         , json_extract(record, '$.kd') kd
+        from slog_entry e
+        where blockHeight between %(bk_lo)s and %(bk_hi)s
+        and file_id = %(file_id)s
+        and type = 'deliver'
+        limit %(limit)s
+        ),
+        detail as (
+        select d.*
+          , json_unquote(json_extract(d.kd, '$[0]')) tag
+          , json_unquote(json_extract(d.kd, '$[1]')) target
+          , json_unquote(json_extract(d.kd, '$[2].method')) method
+          , json_length(json_unquote(json_extract(d.kd, '$[2].args.body')), '$[1].updated') updated
+        from d
+        )
+        select blockHeight, blockTime, crankNum, vatID, deliveryNum
+             , tag
+             , case when tag = 'message' then target else null end target
+             , method, updated
+             , time
+        -- validator-specific: file_id, run_line_lo, line, time
+        from detail
+        -- where method = 'fromBridge'
+        order by blockHeight, crankNum
+        ''', db, params=dict(limit=limit, file_id=file_id, bk_lo=bk_lo, bk_hi=bk_hi))
+    return df
+
+
+# x1 = bank_trace(_db4, bk_hi=68817 + 100)
+# x2 = bank_trace(_db4, bk_lo=69707 - 100)
+# x = pd.concat([x1, x2])
+x = bank_trace(_db4, limit=100)
+show_times(x)
+# -
+
+x.updated.describe()
+
+show_times(x[~x.updated.isnull()]).plot.scatter(x='time', y='updated',
+                                                figsize=(10, 4), alpha=0.45,
+                                                title='Accounts Updated per delivery');
+
+# +
+import json
+
+def notifer_traffic(df):
+    kd = df.record.apply(lambda txt: json.loads(txt)['kd'])
+    dt = kd.apply(lambda k: k[0])
+    method = kd.apply(lambda k: k[2].get('method') if k[0] == 'message' else None)
+    body = kd.apply(lambda k: k[2].get('args', {}).get('body') if k[0] == 'message' else None)
+    body = body.apply(lambda v: json.loads(v) if v else None)
+    updated = body.apply(lambda b: len(b[1].get('updated')) if b else None)
+    # time = pd.as_datetime(df.time.dt.time)
+    df = df.assign(dt=dt, method=method, body=body, updated=updated)
+    dur = df.time.diff()
+    return df.assign(dur=dur)
+
+notifer_traffic(show_times(x2)).drop(columns=['file_id', 'run_line_lo', 'line', 'record', 'body'])
+# -
+
+show_times(x2)
+
+x2.record[0]
+
+len(x2.record[0])
+
+# +
+import json
+
+r = json.loads(x2.record[0])
+body = r['kd'][2]['args']['body']
+x = json.loads(body)
+# print(json.dumps(r, indent=2))
+len(x[1]['updated'])
+# -
+
+x2.record[1]
+
+x2.record[2]
