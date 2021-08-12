f05a44defaba1b3cd45dccc15eff3c2e773001a7
Dan Connolly <dckc@madmode.com>
2021-08-12 11:58:00 -0500

    feat: discord login

--- subm/src/subm.js
+++ subm/src/subm.js
@@ -1,11 +1,17 @@
 /* global require, Buffer */
+// @ts-check
 
+const { assert } = require('console');
 const discord = require('passport-discord');
 
 const { freeze } = Object;
 
-// cribbed from rchain-dbr/o2r/gateway/server/main.js
-function DiscordAPI(get, token) {
+/**
+ * @param {string} token
+ * @param {{ get: typeof import('https').get }} io
+ */
+function DiscordAPI(token, { get }) {
+  // cribbed from rchain-dbr/o2r/gateway/server/main.js
   const host = 'discordapp.com';
   const api = '/api/v6';
   const headers = { Authorization: `Bot ${token}` };
@@ -49,20 +55,116 @@ function DiscordAPI(get, token) {
   });
 }
 
+const Pages = {
+  start: `
+<!doctype html>
+<title>Agoric Testnet Submission</title>
+
+<a href="/auth/discord">login to discord and upload</a>
+
+<hr />
+<a href="dckc">dckc discord details</a>
+`,
+};
+
+/**
+ * @param { T | null | undefined } it
+ * @returns { T }
+ * @template T
+ */
+const the = it => {
+  if (!it) throw Error('must not be falsy');
+  return it;
+};
+
+/**
+ *
+ * @param {Record<string, string | undefined>} env
+ * @param {{
+ *   get: typeof import('https').get,
+ *   express: typeof import('express'),
+ *   passport: typeof import('passport'),
+ * }} io
+ */
+async function main(env, { get, express, passport }) {
+  const host = env.HOST || 'localhost';
+  const port = parseInt(env.PORT || '3000', 10);
+  const base = `http://localhost:${port}`;
+
+  const app = express();
+  app.use(passport.initialize());
+
+  const api = DiscordAPI(the(env.TOKEN), { get });
+  const guild = api.guilds(the(env.GUILD_ID));
+
+  const scopes = ['identify', 'email', 'guilds', 'guilds.join'];
+  const callbackPath = '/auth/discord/callback';
+  const callbackURL = new URL(callbackPath, base).toString();
+
+  passport.serializeUser((user, done) => done(null, user));
+  passport.deserializeUser((obj, done) => done(null, obj));
+
+  passport.use(
+    new discord.Strategy(
+      {
+        clientID: the(env.CLIENT_ID),
+        clientSecret: the(env.CLIENT_SECRET),
+        callbackURL,
+        scope: scopes,
+      },
+      async (accessToken, refreshToken, profile, cb) => {
+        try {
+          const user = await guild.members(profile.id);
+          console.log('@@@user', user);
+          cb(null, `${user.username}#${user.discriminator}`);
+        } catch (err) {
+          cb(err);
+        }
+      },
+    ),
+  );
+
+  app.get('/', (_req, res) => res.send(Pages.start));
+  app.get('/auth/discord', passport.authenticate('discord'));
+  app.get(
+    callbackPath,
+    passport.authenticate('discord', {
+      failureRedirect: '/',
+    }),
+    (_req, res) => {
+      res.redirect('/uploadForm'); // Successful auth
+    },
+  );
+
+  app.get('/dckc', (req, res) => {
+    DiscordAPI(env.TOKEN || '', { get })
+      .guilds(env.GUILD_ID || '')
+      .members(env.USER_ID || '')
+      .then(member => {
+        // res.type('text/plain');
+        res.set('Content-Type', 'text/plain');
+        res.send(JSON.stringify(member, null, 2));
+      })
+      .catch(oops => {
+        console.log(oops);
+      });
+  });
+
+  console.log(base);
+  app.listen(port);
+}
+
 /* global module */
 if (require.main === module) {
   // ocap: Import powerful references only when invoked as a main module.
   /* eslint-disable global-require */
   /* global process */
-  const get = require('https').get;
-  const env = process.env;
-  DiscordAPI(get, env.TOKEN || '')
-    .guilds(env.GUILD_ID || '')
-    .members(env.USER_ID || '')
-    .then(member => {
-      console.log({ member });
-    })
-    .catch(oops => {
-      console.log(oops);
-    });
+  main(
+    { ...process.env },
+    {
+      express: require('express'),
+      passport: require('passport'),
+      get: require('https').get,
+    },
+  ).catch(err => console.error(err));
 }
