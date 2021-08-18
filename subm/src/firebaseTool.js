d9e37a00630408714c5d562bc02fcff07e2377b2
Dan Connolly <dckc@madmode.com>
2021-08-18 00:48:43 -0500

    refactor(subm): prepare firebaseTool for use in subm app
      - add static types

--- subm/src/firebaseTool.js
+++ subm/src/firebaseTool.js
@@ -1,21 +1,48 @@
 /* global Buffer */
 // @ts-check
 
-async function generateCustomToken(uid, admin, firebaseConfig) {
-  const jwt = await admin.auth().createCustomToken(uid);
+const { freeze } = Object; // IOU ses / harden
 
-  const encodedConfig = Buffer.from(JSON.stringify(firebaseConfig)).toString(
-    'base64',
-  );
+/**
+ * @param { TemplateTag } config
+ */
+function getFirebaseConfig(config) {
+  return {
+    apiKey: config`FIREBASE_API_KEY`,
+    authDomain: config`FIREBASE_AUTH_DOMAIN`,
+    databaseURL: config`FIREBASE_DATABASE_URL`,
+    projectId: config`FIREBASE_PROJECT_ID`,
+    appId: config`FIREBASE_APP_ID`,
+  };
+}
 
-  const combinedToken = [encodedConfig, jwt].join('.');
+/**
+ * @param { typeof import('firebase-admin') } admin
+ * @param {ReturnType<typeof getFirebaseConfig>} firebaseConfig
+ */
+function makeFirebaseAdmin(admin, firebaseConfig) {
+  return freeze({
+    init: () => {
+      // "If your code is deployed in the App Engine standard environment for Java, Python or Go, the Admin SDK can use the App Identity service present in that environment to sign custom tokens."
+      // -- https://firebase.google.com/docs/auth/admin/create-custom-tokens#letting_the_admin_sdk_discover_a_service_account
+      admin.initializeApp();
+    },
 
-  console.log(
-    'Please execute:',
-    `curl -s -X PUT --data '${combinedToken}' http://127.0.0.1:3352/push-config`,
-  );
+    /**
+     * @param { string } uid
+     */
+    async generateCustomToken(uid) {
+      const jwt = await admin.auth().createCustomToken(uid);
 
-  return combinedToken;
+      const encodedConfig = Buffer.from(
+        JSON.stringify(firebaseConfig),
+      ).toString('base64');
+
+      const combinedToken = [encodedConfig, jwt].join('.');
+
+      return combinedToken;
+    },
+  });
 }
 
 /**
@@ -33,26 +60,29 @@ const makeConfig = env => {
   };
 };
 
+/**
+ * @param {string[]} args
+ * @param {Record<string, string | undefined>} env
+ * @param {{ admin: typeof import('firebase-admin') }} io
+ */
 async function main(args, env, { admin }) {
   const [uid] = args;
-  // "If your code is deployed in the App Engine standard environment for Java, Python or Go, the Admin SDK can use the App Identity service present in that environment to sign custom tokens."
-  // -- https://firebase.google.com/docs/auth/admin/create-custom-tokens#letting_the_admin_sdk_discover_a_service_account
-  admin.initializeApp();
 
   const config = makeConfig(env);
+  const firebaseConfig = getFirebaseConfig(config);
 
-  const firebaseConfig = {
-    apiKey: config`FIREBASE_API_KEY`,
-    authDomain: config`FIREBASE_AUTH_DOMAIN`,
-    databaseURL: config`FIREBASE_DATABASE_URL`,
-    projectId: config`FIREBASE_PROJECT_ID`,
-    appId: config`FIREBASE_APP_ID`,
-  };
-
-  generateCustomToken(uid, admin, firebaseConfig);
+  const anAdmin = makeFirebaseAdmin(admin, firebaseConfig);
+  anAdmin.init();
+  const combinedToken = await anAdmin.generateCustomToken(uid);
+  console.log(
+    'Please execute:',
+    `curl -s -X PUT --data '${combinedToken}' http://127.0.0.1:3352/push-config`,
+  );
 }
 
 /* global process, require, module */
+module.exports = { makeFirebaseAdmin, getFirebaseConfig, main };
+
 if (require.main === module) {
   main(
     process.argv.slice(2),
