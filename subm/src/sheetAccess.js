3fd6cf3a8c64084b785a3af79e0cb33a2c7591d7
Dan Connolly <dckc@madmode.com>
2021-12-22 22:30:50 -0600

    feat: upsert in Google sheet

--- subm/src/sheetAccess.js
+++ subm/src/sheetAccess.js
@@ -1,5 +1,39 @@
+const range = n => [...Array(n).keys()];
+const takeWhile = (xs, predicate) =>
+  xs.slice(
+    0,
+    xs.findIndex(x => !predicate(x)),
+  );
+
 /**
- *
+ * @param {import('google-spreadsheet').GoogleSpreadsheetWorksheet} sheet
+ * @param {string | number} key
+ * @param {Record<string, string | number>} record
+ */
+const upsert = async (sheet, key, record) => {
+  // load primary key column
+  await sheet.loadCells({
+    startColumnIndex: 0,
+    endColumnIndex: 1,
+  });
+
+  const destIndex = range(sheet.rowCount).find(rowIndex => {
+    if (rowIndex === 0) return false; // header row
+    const { value } = sheet.getCell(rowIndex, 0);
+    return key === value || value === null;
+  });
+  if (destIndex < 0) throw Error('key not found and no available rows');
+  let [row] = await sheet.getRows({ offset: destIndex - 1, limit: 1 });
+  if (row) {
+    Object.assign(row, record);
+    await row.save({ raw: true });
+  } else {
+    row = await sheet.addRow(record);
+  }
+  return row;
+};
+
+/**
  * @param {string[]} argv
  * @param {Record<string, string | undefined>} env
  * @param {Object} io
@@ -7,7 +41,7 @@
  */
 const main = async (argv, env, { GoogleSpreadsheet }) => {
   // Initialize the sheet - doc ID is the long id in the sheets URL
-  const doc = new GoogleSpreadsheet(env.SHEET1_ID); // ISSUE: AMBIENT
+  const doc = new GoogleSpreadsheet(env.SHEET1_ID);
 
   const creds = JSON.parse(env.GCS_SERVICE_ACCOUNT);
   // Initialize Auth - see https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
@@ -15,15 +49,15 @@ const main = async (argv, env, { GoogleSpreadsheet }) => {
 
   await doc.loadInfo(); // loads document properties and worksheets
   console.log(doc.title);
-  //   await doc.updateProperties({ title: 'renamed doc' });
 
-  //   const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
-  //   console.log(sheet.title);
-  //   console.log(sheet.rowCount);
+  const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+  console.log(sheet.title);
+  console.log(sheet.rowCount);
 
-  //   // adding / removing sheets
-  //   const newSheet = await doc.addSheet({ title: 'hot new sheet!' });
-  //   await newSheet.delete();
+  await upsert(sheet, '358096357862408195', {
+    userID: '358096357862408195',
+    email: 'dckc@agoric.com',
+  });
 };
 
 /* global require, process, module */
